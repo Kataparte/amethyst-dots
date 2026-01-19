eb3d07ebadc7da4edc9f2ca8d86b27bd68c29605
Kataparte <101990463+Kataparte@users.noreply.github.com~>
2026-01-18 21:35:04 -0500

    feat: migrate workspace 2 to Neovim and initialize LazyVim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "RRethy/nvim-base16",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "base16-tomorrow-night", -- Or your specific matugen-generated theme name
+    },
+  },
+}
