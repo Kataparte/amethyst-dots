13e0e36bb3a41345642e3ad69bb5483e7b098602
Kataparte <101990463+Kataparte@users.noreply.github.com~>
2026-01-24 23:17:16 -0500

    feat: sync Bebop theme, enable nvim-python & add SnipRun interactive mode

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,1 +1,13 @@
-return {}
+return {
+  {
+    "michaelb/sniprun",
+    branch = "master",
+    build = "sh ./install.sh",
+    config = function()
+      require("sniprun").setup({
+        -- This tells SnipRun to show results in a floating window
+        display = { "FloatingWindow" }, 
+      })
+    end,
+  },
+}
